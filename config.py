0fe9e3aca31e8028613320cf5cef256a0e90f349
Nikita Korshunov <kewka96@gmail.com>
2019-08-11 13:52:10 +0500

    Update config.py

--- config.py
+++ config.py
@@ -10,12 +10,12 @@ SERVER_HOST = os.getenv('HOST', '0.0.0.0')
 SERVER_PORT = int(os.getenv('PORT', '7000'))
 SERVER_ADDRESS = (SERVER_HOST, SERVER_PORT)
 
-# Maximum size of TXD file (1 MB).
-MAX_FILE_SIZE = 1024 * 1024
+# Maximum size of TXD file (16 MB).
+MAX_FILE_SIZE = int(os.getenv('MAX_FILE_SIZE', str(1024 * 1024 * 16)))
 # Buffer size (64 KB).
-BUFFER_SIZE = 1024 * 64
+BUFFER_SIZE = int(os.getenv('BUFFER_SIZE', str(1024 * 64)))
 # Socket timeout (5 seconds).
-SOCKET_TIMEOUT_SECONDS = 5
+SOCKET_TIMEOUT_SECONDS = int(os.getenv('SOCKET_TIMEOUT_SECONDS', '5'))
 
 # Header size (4 bytes).
 HEADER_SIZE = 4
